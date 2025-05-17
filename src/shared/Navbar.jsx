0ebfd60973eca2561788876489b00fef42897d5a
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-05-17 09:44:20 +0600

    Update Navbar.jsx

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -3,8 +3,10 @@ import { useContext } from "react";
 import { AuthContext } from "../Context/AuthProvider";
 import { Link } from "react-router-dom";
 import icon from "../assets/icons/Untitled design (2).png";
+
 export default function Navbar() {
   const { user, logOut } = useContext(AuthContext);
+
   const handleLogout = () => {
     logOut()
       .then(() => {})
@@ -13,125 +15,80 @@ export default function Navbar() {
 
   return (
     <>
-      <div className=" pb-6 sm:pb-8 lg:pb-0 print:hidden">
+      <div className="pb-6 sm:pb-8 lg:pb-0 print:hidden">
         <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-          <header className=" flex items-center justify-between py-4 md:mb-12 md:py-8 xl:mb-0">
+          <header className="flex items-center justify-between py-4 md:mb-12 md:py-8 xl:mb-0">
             <Link
-              href="/"
-              className="inline-flex items-center gap-0 text-2xl font-bold  md:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-slate-300 to-gray-500 "
+              to="/"
+              className="inline-flex items-center gap-0 text-2xl font-bold md:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-slate-300 to-gray-500"
               aria-label="logo"
             >
-              {/* <svg
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                aria-hidden="true"
-                role="img"
-                className="iconify iconify--logos"
-                width="31.88"
-                height="32"
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 256 257"
-              >
-                <defs>
-                  <linearGradient
-                    id="IconifyId1813088fe1fbc01fb466"
-                    x1="-.828%"
-                    x2="57.636%"
-                    y1="7.652%"
-                    y2="78.411%"
-                  >
-                    <stop offset="0%" stopColor="#41D1FF"></stop>
-                    <stop offset="100%" stopColor="#BD34FE"></stop>
-                  </linearGradient>
-                  <linearGradient
-                    id="IconifyId1813088fe1fbc01fb467"
-                    x1="43.376%"
-                    x2="50.316%"
-                    y1="2.242%"
-                    y2="89.03%"
-                  >
-                    <stop offset="0%" stopColor="#FFEA83"></stop>
-                    <stop offset="8.333%" stopColor="#FFDD35"></stop>
-                    <stop offset="100%" stopColor="#FFA800"></stop>
-                  </linearGradient>
-                </defs>
-                <path
-                  fill="url(#IconifyId1813088fe1fbc01fb466)"
-                  d="M255.153 37.938L134.897 252.976c-2.483 4.44-8.862 4.466-11.382.048L.875 37.958c-2.746-4.814 1.371-10.646 6.827-9.67l120.385 21.517a6.537 6.537 0 0 0 2.322-.004l117.867-21.483c5.438-.991 9.574 4.796 6.877 9.62Z"
-                ></path>
-                <path
-                  fill="url(#IconifyId1813088fe1fbc01fb467)"
-                  d="M185.432.063L96.44 17.501a3.268 3.268 0 0 0-2.634 3.014l-5.474 92.456a3.268 3.268 0 0 0 3.997 3.378l24.777-5.718c2.318-.535 4.413 1.507 3.936 3.838l-7.361 36.047c-.495 2.426 1.782 4.5 4.151 3.78l15.304-4.649c2.372-.72 4.652 1.36 4.15 3.788l-11.698 56.621c-.732 3.542 3.979 5.473 5.943 2.437l1.313-2.028l72.516-144.72c1.215-2.423-.88-5.186-3.54-4.672l-25.505 4.922c-2.396.462-4.435-1.77-3.759-4.114l16.646-57.705c.677-2.35-1.37-4.583-3.769-4.113Z"
-                ></path>
-              </svg> */}
-              <img src={icon} alt="" className="h-16" />
+              <img src={icon} alt="Shoishob Logo" className="h-16" />
               <p>Shoishob</p>
             </Link>
 
             <nav className="hidden gap-12 lg:flex">
-              <a href="/" className="text-sm font-semibold text-cyan-500">
+              <Link to="/" className="text-sm font-semibold text-cyan-500">
                 Home
-              </a>
-              <a
-                href="/booking"
+              </Link>
+              <Link
+                to="/booking"
                 className="text-sm font-semibold text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
               >
                 Booking
-              </a>
-
-              <a
-                href="/leaderBoard"
-                className="text-sm font-semibold  text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
+              </Link>
+              <Link
+                to="/leaderBoard"
+                className="text-sm font-semibold text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
               >
                 Leader Board
-              </a>
-              <a
-                href="/about"
-                className="text-sm font-semibold  text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
+              </Link>
+              <Link
+                to="/about"
+                className="text-sm font-semibold text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
               >
                 About
-              </a>
+              </Link>
 
               {user?.uid ? (
                 <>
-                  <a
-                    href="/review"
-                    className="text-sm font-semibold  text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
+                  <Link
+                    to="/review"
+                    className="text-sm font-semibold text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
                   >
                     Review
-                  </a>
-                  <a
-                    href="/dashboard"
-                    className="text-sm font-semibold  text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
+                  </Link>
+                  <Link
+                    to="/dashboard"
+                    className="text-sm font-semibold text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
                   >
                     Dashboard
-                  </a>
+                  </Link>
                   <button
                     onClick={handleLogout}
-                    className="text-sm font-semibold  text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
+                    className="text-sm font-semibold text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
                   >
                     Sign Out
                   </button>
                 </>
               ) : (
-                <a
-                  href="/login"
-                  className="text-sm font-semibold  text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
+                <Link
+                  to="/login"
+                  className="text-sm font-semibold text-gray-400 transition duration-100 hover:text-cyan-500 active:text-indigo-900"
                 >
                   Login
-                </a>
+                </Link>
               )}
             </nav>
 
-            <a
-              href="/contact"
+            <Link
+              to="/contact"
               className="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-sm lg:inline-block"
             >
               Contact Sales
-            </a>
+            </Link>
 
-            <button
-              type="button"
+            <label
               htmlFor="dashboard-drawer"
               className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-sm lg:hidden"
             >
@@ -148,7 +105,7 @@ export default function Navbar() {
                 />
               </svg>
               Menu
-            </button>
+            </label>
           </header>
         </div>
       </div>
