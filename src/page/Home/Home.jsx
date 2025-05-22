3d2dad86509a2c9ec94fe504b49150355a4ab216
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-05-22 09:52:09 +0600

    Update Home.jsx

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,19 +1,18 @@
 import Turf from "./Turf";
 import Bannar from "./Bannar";
 import Featured from "./Featured";
-import EmailCollect from "./EmailCollect";
 import Heading from "./Heading";
-
 
 export default function Home() {
   return (
-    <div className="max-w-7xl mx-auto">
-      <Bannar></Bannar>
-      <Turf></Turf>
-
-      <Heading />
+    <>
+      <div className="max-w-screen-2xl mx-auto px-5 md:px-10">
+        <Bannar></Bannar>
+        <Turf></Turf>
+        <Heading />
+      </div>
       <Featured></Featured>
-      <EmailCollect></EmailCollect>
-    </div>
+      {/* <EmailCollect></EmailCollect> */}
+    </>
   );
 }
