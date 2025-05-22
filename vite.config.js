4f8120b2689a784e3992293e1ee75c57465b559e
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-05-22 16:57:12 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  envPrefix: 'VITE_' // This is actually the default, but good to be explicit
+  envPrefix: 'VITE_',
+  server: {
+    cors: true,
+
+  }
 })
