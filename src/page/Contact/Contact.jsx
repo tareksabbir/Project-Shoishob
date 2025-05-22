2d6590cf7f441d25669d83b21cba110a75aed9eb
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-05-22 12:06:56 +0600

    Update Contact.jsx

--- src/page/Contact/Contact.jsx
+++ src/page/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import Content from "./Content";
 
 export default function Contact() {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl mx-auto min-h-screen my-24">
       <Content></Content>
     </div>
   );
