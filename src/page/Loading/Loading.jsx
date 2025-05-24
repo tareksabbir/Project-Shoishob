ca505b3ddd1059e1f352d44dd471f16580b4cf09
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-05-24 20:57:54 +0600

    Update Loading.jsx

--- src/page/Loading/Loading.jsx
+++ src/page/Loading/Loading.jsx
@@ -1,20 +1,8 @@
-import icon from "../../assets/icons/Untitled design (2).png";
 const Loading = () => {
   return (
     <>
-      <div className="bg-slate-900 w-full min-h-screen flex justify-center items-center">
-        <div className="flex min-h-screen w-full items-center justify-center bg-slate-900">
-          <div className="flex items-center justify-center">
-            <a
-              href="/"
-              className="inline-flex items-center gap-2.5 text-2xl font-bold  md:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-200"
-              aria-label="logo"
-            >
-              <img src={icon} alt="" className="h-16" />
-              Please Wait..
-            </a>
-          </div>
-        </div>
+      <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-500"></div>
       </div>
     </>
   );
