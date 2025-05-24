9cb9a8f2ed2c82fb3b91938647c7728756bd4b62
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-05-24 14:50:13 +0600

    Update About.jsx

--- src/page/About/About.jsx
+++ src/page/About/About.jsx
@@ -1,17 +1,11 @@
-import EmailCollect from "../Home/EmailCollect";
-import AboutCount from "./AboutCount";
-import Advantage from "./Advantage";
-import Motivation from "./Motivation";
-import TamMembers from "./TamMembers";
+
+
+import AboutUs from "./AboutUs";
+
 
 export default function About() {
   return (
-    <div className="max-w-7xl mx-auto">
-      <Motivation></Motivation>
-      <AboutCount></AboutCount>
-      <TamMembers></TamMembers>
-      <Advantage></Advantage>
-      <EmailCollect></EmailCollect>
-    </div>
+
+       <AboutUs></AboutUs>
   );
 }
