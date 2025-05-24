bff7f5b1cefe10dd2ad1d68c7bc040f04f66e485
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-05-24 22:20:32 +0600

    Update About.jsx

--- src/page/About/About.jsx
+++ src/page/About/About.jsx
@@ -6,7 +6,7 @@ import AboutUs from "./AboutUs";
 export default function About() {
   return (
 
-      <section className="max-w-screen-2xl px-5 md:px-20 mx-auto">
+      <section className="max-w-screen-2xl px-5 md:px-10 mx-auto">
          <AboutUs></AboutUs>
       </section>
   );
